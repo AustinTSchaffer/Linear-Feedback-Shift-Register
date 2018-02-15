a8d636adb51cabf0b8541f34977c4bd9322b846f
Austin Schaffer <schaffer.austin.t@gmail.com>
2018-02-15 14:58:04 -0500

    Demonstrated C# F# Interoperability

--- src/CSharpUsage/Program.cs
+++ src/CSharpUsage/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using Microsoft.FSharp.Collections;
+using ShiftRegisters;
+using ShiftRegisters.ConcreteImplementations;
 
 namespace CSharpUsage
 {
@@ -6,7 +11,29 @@ namespace CSharpUsage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This small project demonstrates interoperability between F# and C#.");
+            Console.WriteLine();
+
+            Console.WriteLine("Creating a new shift register, which is type defined in an F# project.");
+            var shiftReg = new ShiftRegister(0x0123456789ABCDEF, 64);
+            Console.WriteLine($"Calling the ShiftRegister's ToString() -> '{shiftReg}'");
+            Console.WriteLine();
+
+            Console.WriteLine("Lists have some interesting interoperability.");
+
+            var taps = ListModule.OfSeq(new List<int>() {3, 5});
+            var xorLFSR = new ExclusiveOrLFSR(1, taps);
+
+            var set = new HashSet<BigInteger>();
+            var iteration = 0;
+            Console.WriteLine("Looping through XOR LFSR until a duplicate value is found...");
+            while (!set.Contains(xorLFSR.State))
+            {
+                iteration++;
+                set.Add(xorLFSR.State);
+                Console.WriteLine($"Iteration #{iteration}: {xorLFSR.State}");
+                xorLFSR.Shift();
+            }
         }
     }
 }
